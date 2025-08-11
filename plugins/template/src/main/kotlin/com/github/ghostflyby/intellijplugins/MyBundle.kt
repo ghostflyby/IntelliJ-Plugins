58ef2874d0566d949a5d29393f531148f6ad6e7c
GitHub Action <action@github.com>
2025-08-11 13:13:33 +0000

    Template cleanup

--- plugins/template/src/main/kotlin/com/github/ghostflyby/intellijplugins/MyBundle.kt
+++ plugins/template/src/main/kotlin/com/github/ghostflyby/intellijplugins/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ghostflyby.intellijplugins
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
-
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+
+rootProject.name = "IntelliJ-Plugins"
 
 // Monorepo: include plugin subprojects under `plugins/`
 file("plugins").listFiles()?.filter { it.isDirectory }?.forEach { pluginDir ->
