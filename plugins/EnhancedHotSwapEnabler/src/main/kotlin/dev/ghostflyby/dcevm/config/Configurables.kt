19d3cfd3d92eb7a4b98532ce5f45b3cea8c49644
ghostflyby <ghostflyby+git@outlook.com>
2025-10-27 15:21:35 +0800

    chore: update copyright config

--- plugins/EnhancedHotSwapEnabler/src/main/kotlin/dev/ghostflyby/dcevm/config/Configurables.kt
+++ plugins/EnhancedHotSwapEnabler/src/main/kotlin/dev/ghostflyby/dcevm/config/Configurables.kt
@@ -1,7 +1,11 @@
 /*
- * Copyright (c) 2025 ghostflyby <ghostflyby+intellij@outlook.com>
+ * Copyright (c) 2025 ghostflyby
+ * SPDX-FileCopyrightText: 2025 ghostflyby
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  *
- * This program is free software; you can redistribute it and/or
+ * This file is part of IntelliJ-Plugins by ghostflyby
+ *
+ * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
@@ -36,8 +40,8 @@ internal sealed class DCEVMConfigurable : Configurable {
 
     final override fun isModified(): Boolean {
         return persistent.enable != state.enable ||
-                persistent.enableHotswapAgent != state.enableHotswapAgent ||
-                persistent.inherit != state.inherit
+            persistent.enableHotswapAgent != state.enableHotswapAgent ||
+            persistent.inherit != state.inherit
     }
 
     final override fun reset() {
@@ -54,8 +58,6 @@ internal sealed class DCEVMConfigurable : Configurable {
     }
 
 }
-
-
 
 
 internal class AppDCEVMConfigurable : DCEVMConfigurable() {
--- plugins/IdeaVimToggleIME/src/main/kotlin/dev/ghostflyby/ideavim/toggleIME/DisableImeInNormal.kt
+++ plugins/IdeaVimToggleIME/src/main/kotlin/dev/ghostflyby/ideavim/toggleIME/DisableImeInNormal.kt
@@ -1,7 +1,11 @@
 /*
- * Copyright (c) 2025 ghostflyby <ghostflyby+intellij@outlook.com>
+ * Copyright (c) 2025 ghostflyby
+ * SPDX-FileCopyrightText: 2025 ghostflyby
+ * SPDX-License-Identifier: LGPL-3.0-or-later
  *
- * This program is free software; you can redistribute it and/or
+ * This file is part of IntelliJ-Plugins by ghostflyby
+ *
+ * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
