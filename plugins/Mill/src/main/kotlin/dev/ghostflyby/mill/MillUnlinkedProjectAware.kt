14b2f9184d26fd21e72c17f1100170dcecaaab0c
ghostflyby <ghostflyby+git@outlook.com>
2026-03-28 00:56:24 +0800

    chore: remove redundant license headers and update verifyPlugin task configuration

--- plugins/Mill/src/main/kotlin/dev/ghostflyby/mill/MillUnlinkedProjectAware.kt
+++ plugins/Mill/src/main/kotlin/dev/ghostflyby/mill/MillUnlinkedProjectAware.kt
@@ -22,28 +22,6 @@
 
 @file:Suppress("UnstableApiUsage")
 
-/*
- * Copyright (c) 2026 ghostflyby
- * SPDX-FileCopyrightText: 2026 ghostflyby
- * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of IntelliJ-Plugins by ghostflyby
- *
- * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see
- * <https://www.gnu.org/licenses/>.
- */
-
 package dev.ghostflyby.mill
 
 import com.intellij.openapi.Disposable
@@ -62,6 +40,11 @@ import java.nio.file.Path
 // ExternalSystemProjectLinkListener is still experimental on 2025.3, but this
 // extension is the public way to surface external-system import guidance after
 // a project is opened.
+//
+// JvmDefaultWithoutCompatibility prevents Kotlin from emitting the deprecated
+// sync bridge for linkAndLoadProject(Project, String), which Marketplace counts
+// as a deprecated API usage in the generated classfile.
+@JvmDefaultWithoutCompatibility
 internal class MillUnlinkedProjectAware : ExternalSystemUnlinkedProjectAware {
     override val systemId = MillConstants.systemId
 
