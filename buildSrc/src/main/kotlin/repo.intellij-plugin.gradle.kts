68ccb398a4b4f5a92b5cf03aa4a24890050b6983
ghostflyby <ghostflyby+git@outlook.com>
2026-05-31 00:42:02 +0800

    build: auto exclude some common libraries from plugin bundle

--- buildSrc/src/main/kotlin/repo.intellij-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/repo.intellij-plugin.gradle.kts
@@ -2,22 +2,6 @@
  * Copyright (c) 2025-2026 ghostflyby
  * SPDX-FileCopyrightText: 2025-2026 ghostflyby
  * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of IntelliJ-Plugins by ghostflyby
- *
- * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see
- * <https://www.gnu.org/licenses/>.
  */
 
 import org.jetbrains.changelog.Changelog
@@ -95,6 +79,18 @@ changelog {
 tasks {
     prepareSandbox {
         disabledPlugins.add("org.jetbrains.completion.full.line")
+        exclude { file ->
+            listOf(
+                "kotlin-stdlib",
+                "kotlin-reflect",
+                "kotlinx",
+                "annotations"
+            ).any {
+                file.name.endsWith("jar") &&
+                        file.name.startsWith(it)
+            }
+
+        }
     }
     publishPlugin { dependsOn(patchChangelog) }
 
