c0dbc7136cd3395ff065dcaad2d14594b3f451ff
ghostflyby <ghostflyby+git@outlook.com>
2026-06-23 00:29:57 +0800

    chore: update ABI and configure the build after rebase

--- plugins/Mill/build.gradle.kts
+++ plugins/Mill/build.gradle.kts
@@ -2,25 +2,7 @@
  * Copyright (c) 2026 ghostflyby
  * SPDX-FileCopyrightText: 2026 ghostflyby
  * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of IntelliJ-Plugins by ghostflyby
- *
- * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see
- * <https://www.gnu.org/licenses/>.
  */
-
-import org.jetbrains.intellij.platform.gradle.tasks.VerifyPluginTask
 
 plugins {
     id("repo.intellij-plugin")
@@ -32,13 +14,8 @@ dependencies {
     implementation(project(":modules:intellij-shared"))
 
     intellijPlatform {
+        pluginComposedModule(project(":modules:intellij-shared"))
         bundledPlugin("com.intellij.java")
         compatiblePlugin("org.intellij.scala")
-    }
-}
-
-tasks.named("verifyPlugin", VerifyPluginTask::class) {
-    failureLevel = VerifyPluginTask.FailureLevel.entries.filterNot {
-        it == VerifyPluginTask.FailureLevel.EXPERIMENTAL_API_USAGES
     }
 }
