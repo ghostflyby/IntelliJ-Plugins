09a44ad66943fc77f9e04484ea74be02c5bae169
ghostflyby <ghostflyby+git@outlook.com>
2026-06-30 15:39:49 +0800

    chore: update copyright

--- plugins/Mill/src/main/kotlin/dev/ghostflyby/mill/script/MillBuildScriptLibraryRootsProvider.kt
+++ plugins/Mill/src/main/kotlin/dev/ghostflyby/mill/script/MillBuildScriptLibraryRootsProvider.kt
@@ -2,35 +2,19 @@
  * Copyright (c) 2026 ghostflyby
  * SPDX-FileCopyrightText: 2026 ghostflyby
  * SPDX-License-Identifier: LGPL-3.0-or-later
- *
- * This file is part of IntelliJ-Plugins by ghostflyby
- *
- * IntelliJ-Plugins by ghostflyby is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see
- * <https://www.gnu.org/licenses/>.
  */
 
 package dev.ghostflyby.mill.script
 
+import com.intellij.navigation.ItemPresentation
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.AdditionalLibraryRootsProvider
 import com.intellij.openapi.roots.SyntheticLibrary
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.navigation.ItemPresentation
 import dev.ghostflyby.mill.MillConstants
+import org.jetbrains.plugins.scala.icons.Icons
 import java.nio.file.Path
 import javax.swing.Icon
-import org.jetbrains.plugins.scala.icons.Icons
 
 internal class MillBuildScriptLibraryRootsProvider : AdditionalLibraryRootsProvider() {
     override fun getAdditionalProjectLibraries(project: Project): Collection<SyntheticLibrary> {
