6e5c8ad6e90350f21ac24528780b07403043ea1a
ghostflyby <ghostflyby+git@outlook.com>
2026-07-28 23:28:04 +0800

    fix: remove redundant spotlessHarness.close() call in test

--- plugins/SpotlessIntegration/src/test/kotlin/dev/ghostflyby/spotless/SpotlessProjectServiceTest.kt
+++ plugins/SpotlessIntegration/src/test/kotlin/dev/ghostflyby/spotless/SpotlessProjectServiceTest.kt
@@ -1084,8 +1084,6 @@ internal class SpotlessProjectServiceTest {
 
         assertTrue(spotless.canFormat(virtualFile))
 
-        spotlessHarness.close()
-
         assertTrue(waitUntil { provider.completionCount.get() == 1 })
         assertEquals(1, stopCount.get())
         assertFalse(spotless.hasRunningDaemons())
